f998fd47757ef5bb9656702fa2ed5527dbd2cd8a
4techguns <4techgunsalt@gmail.com>
2022-05-01 09:07:26 -0230

    lots of refactors + additions

---
+++ components/Scaffold.js
@@ -0,0 +1,96 @@
+import Head from 'next/head'
+import Metas from '../components/Metas'
+import Image from 'next/image'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { House } from '@mui/icons-material';
+import Alert from '@mui/material/Alert';
+
+
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+import { useRouter } from 'next/router'
+
+
+const drawerWidth = 240;
+
+export default function Scaffold({ children, isLoggedIn, optionalAuth }) {
+  const router = useRouter()
+  return (
+    <>
+      <Head>
+        <Metas />
+      </Head>
+      <ThemeProvider theme={darkTheme}>
+      <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        
+        variant="permanent"
+        anchor="left"
+      >
+        <Toolbar sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          <Image src="/AnoletLogoSmall.png" alt="Anolet logo" width="64" height="64"></Image>
+        </Toolbar>
+        <Divider />
+        {/* main pages will be put here, e.g. home, games, etc. */}
+        {isLoggedIn ? <>
+          <List>
+            <ListItem button key="Home" component="a" onClick={(e) => {
+              router.push("/");
+            }}>
+              <ListItemIcon>
+                <House />
+              </ListItemIcon>
+              <ListItemText primary="Home" />
+            </ListItem>
+          </List>
+          <Divider />
+        </> : <></> }
+        <List>
+        <ListItem button key="Log in/Sign up">
+          <ListItemIcon>
+            <AccountCircleIcon />
+          </ListItemIcon>
+          <ListItemText primary="Log in/Sign up" />
+        </ListItem>
+        </List>
+      </Drawer>
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
+      >
+        { isLoggedIn ? <></> : <Alert severity="warning">Some site features require logging in. You will be able to explore content on this page, but stuff like playing games and creating content will not work. Register an account to join the fun!</Alert> }
+        <br></br>
+        { children }
+      </Box>
+    </Box>
+    </ThemeProvider>
+    </>
+  )
+}
---
+++ pages/users/[id].js
@@ -0,0 +1,34 @@
+import Head from 'next/head'
+import Metas from '../../components/Metas'
+import Toolbar from '@mui/material/Toolbar';
+import Image from 'next/image'
+import * as React from 'react';
+import Scaffold from '../../components/Scaffold';
+
+import { useRouter } from 'next/router'
+
+import Typography from '@mui/material/Typography';
+
+export default function Home() {
+  const router = useRouter()
+  const { id } = router.query
+
+  return (
+    <>
+      <Head>
+        <Metas />
+      </Head>
+      
+      <Scaffold>
+        <Toolbar>
+            <Typography variant="h3">
+                User Name
+            </Typography>
+        </Toolbar>
+        <Typography paragraph>
+          You are now querying ID {id}!
+        </Typography>
+      </Scaffold>
+    </>
+  )
+}
